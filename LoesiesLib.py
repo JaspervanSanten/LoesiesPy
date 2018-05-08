05570d0e2d41c3bb60b7df494750779693f94156
vansanten <vansanten@gmail.com>
2018-05-08 07:48:08 +0200

    Added Bout searching for animal activity for Janneke

--- LoesiesLib.py
+++ LoesiesLib.py
@@ -282,3 +282,185 @@ def AverageMatrices(filePathList, output_file, id_label):
                                                float(duration.microseconds/1000)))
     print()
 # end AverageMatrices
+
+
+from enum import Enum
+import os
+
+###
+#   Create comparison matrix
+#   input_file: The file that contains the data. Built up with rows as entries; columns are the
+#               variables. One column contains the ID of the entry.
+#   output_file:The file whereto the comparison matrix will be written.
+#   id_label:     One of the columns must be a unique ID for the entries. This specifies the label.
+#   headers:    All columns are labeled with headers. If the file does not contain the headers,
+#               specify them in the variable headers.
+###
+def FindBouts(input_file, output_file, id_label, score_level, headers = ''):
+
+    # Settings
+    entryTime = 15
+    class ActBout(Enum):
+        REST = 0
+        WAKE = 1
+
+    if output_file=='':
+        fileSet = os.path.splitext(input_file)
+        output_file = '{0}_out{1}'.format(fileSet[0],fileSet[1])
+    # dbg_file = open('debug_output','wt')
+    print('Processing score files for bouts:')
+    print('\t- input file:\t{0}'.format(input_file))
+    print('\t- output file:\t{0}'.format(output_file))
+    print('')
+    start_time = datetime.datetime.now()
+
+    # Open input file two times
+    csvInFile = 0
+    try:
+        csvInFile = open(input_file, 'rt', encoding='windows-1252')
+    except IOError:
+        print('Error: Could not find input file {0}.'.format(input_file))
+        exit()
+
+    # Try to see if file has a header.
+
+    data_reader = csv.reader(csvInFile, delimiter=';', quotechar='\'')
+    file_has_headers = csv.Sniffer().has_header(csvInFile.read(1024))
+
+    # Read the headers
+    if file_has_headers:
+        if headers != '':
+            print('Error: Headers were already defined. So now they are defined twice. Exciting!')
+            exit()
+        csvInFile.seek(0)
+        headers = next(data_reader)
+        print('Using headers:\n\t{0}'.format(headers))
+        # print(headers)
+
+    # Check if the headers were properly specified.
+    if headers == '':
+        print('Error: No headers were specified. Exciting!')
+        exit()
+    else:
+        for label in headers:
+            if headers.count(label)!=1:
+                print('Error: The headers contain at least one duplicate entry({0}). Exciting!'.format(label))
+                exit()
+
+    # Check if the id_label was properly specified.
+    if id_label == '':
+        print('Error: No id_label was specified. Exciting!'.format(id_label))
+        exit()
+    elif headers.count(id_label)<1:
+        print('Error: The id_label({0}) was not found in the header list. Exciting!\n({1})'.format(id_label, headers))
+        exit()
+    elif headers.count(id_label)>1:
+        print('Error: The id_label({0}) was found more than once in the header list. Exciting!\n({1})'.format(id_label, headers))
+        exit()
+
+    # Open input file as dictReader.
+    dict_reader = csv.DictReader(csvInFile, delimiter=';', fieldnames=headers)
+
+    # Open output file for writing text.
+    if not general_lib.ensure_dir(output_file, True):
+        print('Error: Could not create output directory for file {0}.'.format(output_file))
+
+    csvOutFile = 0
+    try:
+        csvOutFile = open(output_file, 'wt')
+    except IOError:
+        print('Error: Could not open output file {0}.'.format(output_file))
+        exit()
+
+
+    #Open output
+    outBoutList = []
+    BoutsCountList = [0,0]
+    BoutsTotalTImeList = [0,0]
+    BoutsMeanTImeList = [0,0]
+
+    # Print output headers to file
+    outBoutList.append('BType')
+    outBoutList.append('time')
+    outBoutList.append('raw')
+    outBuff = ';'.join(outBoutList)
+    outBuff = outBuff +'\n'
+    csvOutFile.write(outBuff)
+    outBoutList = []
+
+
+    rawActList = []
+    timeBout = 0
+    firstAct = True
+    # newAct = 0
+    # currAct = 0
+    outBuff = ''
+    for in_row in dict_reader:
+        # print('OUT: {0}'.format(in_row[id_label]))
+        if int(in_row[id_label])<score_level:
+            newAct = 0
+            # print('low')
+        else:
+            newAct = 1
+            # print('high')
+
+        if (firstAct):
+            currAct = newAct
+            outBuff = '{0} : '.format('wake' if currAct>=score_level else 'rest')
+            outBoutList.append('{0}'.format('wake' if currAct>=score_level else 'rest'))
+            firstAct = False
+
+        if currAct != newAct:
+            # print('-bout-\n')
+            # New bout found
+            outBoutList.append('{0}'.format(timeBout))
+            rawBuff = '['+ ','.join(rawActList)+']'
+            outBoutList.append(rawBuff)
+            outBuff = ';'.join(outBoutList)
+            outBuff = outBuff +'\n'
+            csvOutFile.write(outBuff)
+
+            BoutsCountList[currAct] = BoutsCountList[currAct] + 1
+            BoutsTotalTImeList[currAct] = BoutsTotalTImeList[currAct] + timeBout
+            BoutsMeanTImeList[currAct] = BoutsTotalTImeList[currAct] / BoutsCountList[currAct]
+
+            timeBout = 0
+            outBoutList = []
+            rawActList = []
+            currAct = newAct
+            outBoutList.append('{0}'.format('wake' if currAct>=score_level else 'rest'))
+
+        rawActList.append('{0}'.format(str(in_row[id_label])))
+        timeBout = timeBout + entryTime
+
+    # Last bout found
+    outBoutList.append('{0}'.format(timeBout))
+    rawBuff = '['+ ','.join(rawActList)+']'
+    outBoutList.append(rawBuff)
+    outBuff = ';'.join(outBoutList)
+    outBuff = outBuff +'\n'
+    csvOutFile.write(outBuff)
+
+    BoutsCountList[currAct] = BoutsCountList[currAct] + 1
+    BoutsTotalTImeList[currAct] = BoutsTotalTImeList[currAct] + timeBout
+    BoutsMeanTImeList[currAct] = BoutsTotalTImeList[currAct] / BoutsCountList[currAct]
+
+    timeBout = 0
+
+    outBuff = '\n'
+    outBuff = outBuff + 'Number of bouts:\n'
+    outBuff = outBuff + '\t- rest :\t{0}\n'.format(BoutsCountList[ActBout.REST.value])
+    outBuff = outBuff + '\t- wake :\t{0}\n'.format(BoutsCountList[ActBout.WAKE.value])
+    outBuff = outBuff +'\n'
+    outBuff = outBuff + 'Total time per bout type:\n'
+    outBuff = outBuff + '\t- rest :\t{0}\n'.format(BoutsTotalTImeList[ActBout.REST.value])
+    outBuff = outBuff + '\t- wake :\t{0}\n'.format(BoutsTotalTImeList[ActBout.WAKE.value])
+    outBuff = outBuff +'\n'
+    outBuff = outBuff + 'Mean time per bout type:\n'
+    outBuff = outBuff + '\t- rest :\t{0}\n'.format(BoutsMeanTImeList[ActBout.REST.value])
+    outBuff = outBuff + '\t- wake :\t{0}\n'.format(BoutsMeanTImeList[ActBout.WAKE.value])
+    csvOutFile.write(outBuff)
+
+    print(outBuff)
+
+# end
